99bb21757d5cae94e3c0a53c781a5e0d02dc9618
sanyath007 <sanyath007@gmail.com>
2021-06-01 10:06:07 +0700

    Modify Login page component and create main css of app

--- client/src/pages/login/Login.js
+++ client/src/pages/login/Login.js
@@ -1,10 +1,45 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './Login.css';
 
 const Login = () => {
+    const email = useRef();
+    const password = useRef();
+
     return (
-        <div className="login">            
-            <h3>Login Page</h3>
+        <div className="login">
+            <div className="loginWrapper">
+                <div className="loginLeft">
+                    <h3 className="loginLogo">Lamasocial</h3>
+                    <span className="loginDesc">
+                        Connect with friends and the world around you on Lamasocial.
+                    </span>
+                </div>
+                <div className="loginRight">
+                    <form className="loginBox">
+                        <input
+                            type="email"
+                            ref={email}
+                            className="loginInput"
+                            placeholder="Email"
+                            required
+                        />
+                        <input
+                            type="password"
+                            ref={password}
+                            className="loginInput"
+                            placeholder="Password"
+                            required
+                        />
+                        <button className="loginButton">
+                            {false ? "" : "Login"}
+                        </button>
+                        <span className="loginForget">Forget Password?</span>
+                        <button className="loginRegisterButton">
+                            {false ? "" : "Create a new account"}
+                        </button>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
